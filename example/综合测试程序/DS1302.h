547e4006f1527f19eb79bd45430042ad0ebde77b
Old Sheep <62084673+Nixiujiade@users.noreply.github.com>
2025-09-28 15:18:27 +0800

    Delete example/综合测试程序 directory

--- example/综合测试程序/DS1302.h
+++
@@ -1,6 +0,0 @@
-
-extern void DS1302_Init();//声明初始化函数
-extern void DS1302_WriteByte(unsigned char command,Data);//声明单字节写入函数
-extern void DS1302_ReadByte(unsigned char command);//声明单字节读出函数
-extern void DS1302_SetTime(unsigned char Date[]);//声明设置内部时间函数
-extern void DS1302_ReadTime(unsigned char Date[]);//声明读取内部时间函数
